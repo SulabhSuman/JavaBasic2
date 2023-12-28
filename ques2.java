2241b0d41c6d04f66f09d66b8809652280754053
SulabhSuman <12rudra2002@gmail.com>
2023-12-29 00:37:53 +0530

    ques2

---
+++ ques2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ques2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for one side of the square:");
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println("area of the square is :" + area);
+    }
+
+}
