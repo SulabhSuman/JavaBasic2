f8866e67b948a90fb0de7a026321c70d721b384a
SulabhSuman <12rudra2002@gmail.com>
2023-12-28 22:46:21 +0530

    first commit

---
+++ javaByApnaCollege/basic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = 'a'; // 97
+        char b = 'b';// 98
+        System.out.println((int) a);
+        System.out.println((int) b);
+        System.out.println(b - a);
+    }
+}
---
+++ javaByApnaCollege/ques1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int average = (A + B + C) / 3;
+        System.out.println("Average is:" + average);
+    }
+}
